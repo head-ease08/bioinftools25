e75bd7978ab9e8fb106d0d962ecb7151fce90007
Mark Mirny <mark.mirny@yandex.ru>
2025-10-12 21:05:13 +0300

    Misc indentation fixes

--- modules/transcribe.py
+++ modules/transcribe.py
@@ -1,7 +1,7 @@
 def transcribe(sequence:str) -> str:
-"""
-Reads string DNA sequence and returns transcribed RNA string.
-Reads string RNA sequence and returns transcribed RNA string.
-"""
+    """
+    Reads string DNA sequence and returns transcribed RNA string.
+    Reads string RNA sequence and returns transcribed RNA string.
+    """
     return sequence.replace("t", "u").replace("T", "U")
 
